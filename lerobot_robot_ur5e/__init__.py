18de77a3111d82947d5859b930f46682ab6cd9a2
rudra-8000 <rudra.research8000@gmail.com>
2026-03-23 15:23:03 +0800

    Delete lerobot_robot_ur5e directory

--- lerobot_robot_ur5e/__init__.py
+++
@@ -1,3 +0,0 @@
-from .lerobot_robot_ur5e import UR5E, UR5EConfig
-
-__all__ = ["UR5E", "UR5EConfig"]
--- lerobot_robot_ur5e/lerobot_robot_ur5e/__init__.py
+++
@@ -1,4 +0,0 @@
-from .config_ur5e import UR5EConfig
-from .ur5e import UR5E
-
-__all__ = ["UR5E", "UR5EConfig"]
